334b261215d703710c6cfdedc9077746702d3fd4
xiejiayun <bestyxie@tungee.com>
2022-06-27 20:13:36 +0800

    feat: 上传文件

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const Koa = require('koa');
 const Route = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const multer = require('@koa/multer')
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 const path = require('path')
+const cors = require('koa2-cors');
 
 const upload = multer()
 
@@ -18,19 +19,23 @@ router.get('/users', (ctx, next) => {
 .get('/', (ctx, next) => {
   ctx.body = 'Wendy'
 })
-.post('/upload', upload.single('avatar'), async (ctx, next) => {
+.post('/upload', upload.single('file'), async (ctx, next) => {
   console.log(ctx.request.file)
-  console.log(path.resolve(__dirname, './uploaed-assets'))
-  fs.writeFileSync(path.resolve(__dirname, `./uploaed-assets`), ctx.request.file.buffer)
-  const reader = fs.createReadStream(ctx.request.file.buffer)
-  const upStream = fs.createWriteStream(path.resolve(__dirname, `./uploaed-assets/${ctx.request.file.originalname}`))
-  reader.pipe(upStream)
-  // await fs.writeFile(path.resolve(__dirname, `./uploaed-assets/${ctx.request.file.filename}`) , ctx.request.file.buffer)
-  console.log(ctx.body)
-  ctx.response.body = 'hhhh'
+  console.log(path.resolve(__dirname, 'uploaed-assets'))
+  await fs.writeFile(ctx.request.file.originalname , ctx.request.file.buffer)
+  ctx.body = 'uploaded'
+  next()
 })
 
 app
+.use(cors({
+  origin: '*',
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
 .use(bodyParser())
 .use(router.routes())
 .use(router.allowedMethods())
